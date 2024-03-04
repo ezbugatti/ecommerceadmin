ed028be558ac8ae4d2026838aafa748b380ab580
ezbugatti <rgun.turuu@gmail.com>
2024-03-04 18:23:52 +0800

    first

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="yem7vwhq">
         {({ open }) => {
           const onClick = () => {
             open();
